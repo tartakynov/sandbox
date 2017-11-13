9f4644b3cf4fbb8bfa6e80e40297d51b2b45a034
Artem Tartakynov <artem.tad@gmail.com>
2017-11-13 15:36:06 +0700

    working on the problem

--- hackerrank/algorithms/np-complete/spies-revised.py
+++ hackerrank/algorithms/np-complete/spies-revised.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import sys
 from random import shuffle
 
 
@@ -18,11 +19,27 @@ def random_solution(n):
     return solution
 
 
-def number_of_blown_covers(solution):
+def collinear(x1, y1, x2, y2, x3, y3):
+    """
+    Check if three points are collinear
+    """
+
+    return (y1 - y2) * (x1 - x3) == (y1 - y3) * (x1 - x2)
+
+
+def number_of_blown_covers(n, solution):
     """
     Total number of blown covers
     """
-    return 0
+
+    count = 0
+    for x1 in xrange(n):
+        for x2 in xrange(x1 + 1, n):
+            for x3 in xrange(x2 + 1, n):
+                y1, y2, y3 = solution[x1], solution[x2], solution[x3]
+                if collinear(x1, y1, x2, y2, x3, y3):
+                    count += 1
+    return count
 
 
 def hill_climber(n):
@@ -30,20 +47,19 @@ def hill_climber(n):
     Runs until either solution is found (no blown covers) or we are stuck at a local optima
     """
     solution = random_solution(n)
-    current_score = number_of_blown_covers(solution)
+    current_score = number_of_blown_covers(n, solution)
     while current_score > 0:
         stuck = True
         for i in xrange(n):
             for j in xrange(i + 1, n):
                 transition = solution[:]
                 transition[i], transition[j] = solution[j], solution[i]
-                next_score = number_of_blown_covers(transition)
+                next_score = number_of_blown_covers(n, transition)
                 if next_score < current_score:
                     solution = transition
                     current_score = next_score
                     stuck = False
                     break
-
         if stuck:
             break
 
@@ -61,7 +77,9 @@ def print_grid(out, n, solution):
 
 
 def main():
-    print hill_climber(11)
+    n, score, solution = hill_climber(7)
+    print solution
+    print_grid(sys.stdout, n, solution)
 
 
 if __name__ == "__main__":
