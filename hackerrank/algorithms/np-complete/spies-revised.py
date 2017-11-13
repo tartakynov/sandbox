17eca6530b764ab3f37d2a053ae5fbfba97a694e
Artem Tartakynov <artem.tad@gmail.com>
2017-11-13 17:05:23 +0700

    sometimes works

--- hackerrank/algorithms/np-complete/spies-revised.py
+++ hackerrank/algorithms/np-complete/spies-revised.py
@@ -5,7 +5,7 @@ from random import shuffle
 
 """
 https://www.hackerrank.com/challenges/spies-revised/problem
-Solved with hill climbing technique
+Trying to solve with hill climbing technique, although it's very slow with bigger N
 """
 
 
@@ -13,12 +13,13 @@ def random_solution(n):
     """
     Init random solution
     """
+
     solution = range(n)
     shuffle(solution)
     return solution
 
 
-def collinear(x1, y1, x2, y2, x3, y3):
+def are_collinear(x1, y1, x2, y2, x3, y3):
     """
     Check if three points are collinear
     """
@@ -26,7 +27,7 @@ def collinear(x1, y1, x2, y2, x3, y3):
     return (y1 - y2) * (x1 - x3) == (y1 - y3) * (x1 - x2)
 
 
-def diagonal(x1, y1, x2, y2):
+def are_diagonal(x1, y1, x2, y2):
     """
     Check if the line between the two points is at a 45 degree angle to the horizontal
     """
@@ -42,44 +43,45 @@ def number_of_blown_covers(n, solution):
     count = 0
     for x1 in xrange(n):
         for x2 in xrange(x1 + 1, n):
+            if are_diagonal(x1, solution[x1], x2, solution[x2]):
+                count += 1
             for x3 in xrange(x2 + 1, n):
-                y1, y2, y3 = solution[x1], solution[x2], solution[x3]
-                if collinear(x1, y1, x2, y2, x3, y3):
+                if are_collinear(x1, solution[x1], x2, solution[x2], x3, solution[x3]):
                     count += 1
-                if x3 == x2 + 1:
-                    if diagonal(x1, y1, x2, y2):
-                        count += 1
     return count
 
 
 def hill_climber(n):
     """
-    Runs until either solution is found (no blown covers) or we are stuck at a local optima
+    Runs until solution is found (though it can stuck at a local optima)
     """
+
     solution = random_solution(n)
     current_score = number_of_blown_covers(n, solution)
     while current_score > 0:
         stuck = True
         for i in xrange(n):
             for j in xrange(i + 1, n):
-                transition = solution[:]
-                transition[i], transition[j] = solution[j], solution[i]
-                next_score = number_of_blown_covers(n, transition)
+                solution[i], solution[j] = solution[j], solution[i]
+                next_score = number_of_blown_covers(n, solution)
                 if next_score < current_score:
-                    solution = transition
+                    print current_score, next_score
                     current_score = next_score
                     stuck = False
-                    break
+                else:
+                    solution[i], solution[j] = solution[j], solution[i]
+
         if stuck:
             break
 
     return n, current_score, solution
 
 
-def print_grid(out, n, solution):
+def print_grid(n, solution, out=sys.stdout):
     """
     Prints the grid for debug
     """
+
     for i in xrange(n):
         for j in xrange(n):
             out.write('S ' if solution[i] == j else '* ')
@@ -87,12 +89,8 @@ def print_grid(out, n, solution):
 
 
 def main():
-    n, score, solution = hill_climber(11)
-    print_grid(sys.stdout, n, solution)
-    print score
-    # n = 11
-    # solution = [3, 7, 0, 10, 5, 9, 1, 4, 2, 8, 6]
-    # print print_grid(sys.stdout, n, solution)
+    n, score, solution = hill_climber(49)
+    print n, score, map(lambda x: x + 1, solution)
 
 
 if __name__ == "__main__":
