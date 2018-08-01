ee3b3d3db452fc192fd24ac993b8b525c7f51177
Artem Tartakynov <artem.tad@gmail.com>
2018-08-01 19:53:38 +0700

    added naive_overlap_map function

--- coursera/dna-sequencing/week3/overlap.py
+++ coursera/dna-sequencing/week3/overlap.py
@@ -2,6 +2,8 @@
 Genome sequences overlap
 """
 
+from itertools import permutations
+
 def overlap(a, b, min_length=3):
     start = 0
     while True:
@@ -12,8 +14,17 @@ def overlap(a, b, min_length=3):
             return len(a) - start
         start += 1
 
+def naive_overlap_map(reads, min_length=3):
+    overlaps = {}
+    for a, b in permutations(reads, 2):
+        overlap_length = overlap(a, b, min_length)
+        if overlap_length > 0:
+            overlaps[(a, b)] = overlap_length
+    return overlaps
+
 def main():
     print overlap('ATTCGACCTATCCTTGCG', 'CTTGCGCAGCTCGAGA')
+    print naive_overlap_map(['ATTCGACCT', 'GACCTATCCTT', 'CTTGCGCAGCTCGAGA'])
 
 if __name__ == '__main__':
     main()
